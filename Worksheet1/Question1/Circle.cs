bb0edd87fb97ab5cb9e2751afde6e33048d81e13
Cedric-Deguara <56271410+Cedric-Deguara@users.noreply.github.com>
2020-10-27 11:15:13 +0100

    Question 1 done

---
+++ Worksheet1/Question1/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question1
+{
+    class Circle
+    {
+        //attributes 
+        public double radius; //radius is a field for the Circle type
+        
+        /* 
+         * GetReadius() is a method called for circle objects to display the circle;s radius on the console
+         * It has no return type (void) and no parementers (no value between brackets).
+        */
+        //method
+        public void GetRadius() //method definition - creating our method
+        {
+            Console.WriteLine("The radius for this circle is " + radius);
+         
+        }
+
+    }
+}
---
+++ Worksheet1/Question1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question1
+{
+    class Program
+    {  
+
+        static void Main(string[] args)
+        {
+            Circle circleGetRadius = new Circle();//crearinf an instance of circle this, circleGetRadius is a new actual circle which will have its own particular values
+            Circle circleGetRadius2 = new Circle();
+            /*
+             * To access fields and methods from an object we need to use the following format: 
+             * objectName.fieldsName
+             * objectName.MethodName()
+             */
+
+            circleGetRadius.radius = 2.5;
+            circleGetRadius2.radius = 3;
+
+            Console.WriteLine("Circle 1: ");
+            circleGetRadius.GetRadius();
+
+            Console.WriteLine("Circle 2: ");
+            circleGetRadius2.GetRadius();
+            
+            Console.ReadKey();//method call 
+        }
+    }
+}
