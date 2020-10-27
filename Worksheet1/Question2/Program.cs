6900b1b62d5903582e44910654b0532b0d723abf
Cedric-Deguara <56271410+Cedric-Deguara@users.noreply.github.com>
2020-10-27 11:37:43 +0100

    Continued Q2

--- Worksheet1/Question2/Program.cs
+++ Worksheet1/Question2/Program.cs
@@ -10,6 +10,20 @@ namespace Question2
     {
         static void Main(string[] args)
         {
-        }
+            Rectangle rect1 = new Rectangle();
+            Rectangle rect2 = new Rectangle();
+
+            Console.Write("Please enter the length for rectangle 1: ");
+
+            /*
+             * When a method reitrns a value the format is: 
+             * variable = method()
+             * so method() will be converted to the reutnrd value and the reutned value will be saved in the variable
+             */
+
+            rect1.length = Convert.ToDouble(Console.ReadLine());
+
+  
+            
     }
 }
--- Worksheet1/Question2/Rectangle.cs
+++ Worksheet1/Question2/Rectangle.cs
@@ -8,6 +8,21 @@ namespace Question2
 {
     class Rectangle
     {
-        
+        public double length;
+        public double width; 
+
+        public void CalculateArea()
+        {
+            double area = length + width;
+
+            Console.WriteLine("The are for this rectangle is " + area);
+        }
+
+        public void CalculatePerimeter()
+        {
+            double perimeter = (length * 2) + (width * 2);
+
+            Console.WriteLine("The perimeter for this rectangle is " + perimeter);
+        }
     }
 }
